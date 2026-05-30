575149b354654c0d671e0f72ab14e9898a3bca5c
Hassanecbl0 <wm2pqtxxvs@privaterelay.appleid.com>
2026-05-30 13:53:58 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,19 +1,16 @@
 from pathlib import Path
 
-# ====================== CONFIGURATION DU RESTAURANT ======================
 RESTAURANT_NAME: str = "Taste of Africa"
 RESTAURANT_ADDRESS: str = "Ouagadougou, Burkina Faso"
 TAX_RATE: float = 0.18          # TVA 18%
 CURRENCY: str = "XOF"
 CURRENCY_SYMBOL: str = "F CFA"
 
-# ====================== CHEMINS DES FICHIERS ======================
 DATA_DIR = Path("data")
 MENU_FILE: Path = DATA_DIR / "menu.json"
 ORDERS_FILE: Path = DATA_DIR / "orders.txt"
 STAFF_FILE: Path = DATA_DIR / "staff.csv"
 
-# ====================== DONNÉES MÉTIER ======================
 ORDER_STATUS: tuple[str, ...] = (
     "waiting", 
     "in preparation", 
@@ -36,6 +33,5 @@ MENU_CATEGORIES: tuple[str, ...] = (
     "drink"
 )
 
-# ====================== LIMITES & CONTRAINTES ======================
 MAX_TABLE_NUMBER: int = 20
 MIN_ORDER_AMOUNT: float = 500.0
